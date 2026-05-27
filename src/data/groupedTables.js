2aa429e951faa287568abdff48aa7fd5d5485a9e
CorduroyTheBear <candyunicornsnumber1@gmail.com>
2026-05-27 18:12:07 -0400

    Added bundle tables

--- src/data/groupedTables.js
+++ src/data/groupedTables.js
@@ -1,5 +1,14 @@
 // Export tables
 
+// Bundles
+import jojaBundle from "./JsonFiles/Bundles/abandonedJoja.json";
+import boilerBundles from "./JsonFiles/Bundles/boiler.json";
+import bulletinBunddles from "./JsonFiles/Bundles/bulletinBoard.json";
+import craftsBundles from "./JsonFiles/Bundles/crafts.json";
+import fishBundles from "./JsonFiles/Bundles/fishTank.json";
+import pantryBundles from "./JsonFiles/Bundles/pantry.json";
+import vaultBunddles from "./JsonFiles/Bundles/vault.json";
+
 // Traveling Merchant
 import tm_Sun from "./JsonFiles/TravelingMerchant/tm_Sun.json";
 import tm_Mon from "./JsonFiles/TravelingMerchant/tm_Mon.json";
@@ -9,9 +18,27 @@ import tm_Thu from "./JsonFiles/TravelingMerchant/tm_Thu.json";
 import tm_Fri from "./JsonFiles/TravelingMerchant/tm_Fri.json";
 import tm_Sat from "./JsonFiles/TravelingMerchant/tm_Sat.json";
 
+// {id: "", heading: "", className: "tableLevel-2_Tables", data: },
 
 export const GROUPED_TABLES = 
 {
+
+   bundles:
+   {
+        heading: "Bundles",
+        className: "tableLevel-2_Heading",
+        groups:
+        [
+            {id: "jojaBundle", heading: "Joja Mart", className: "tableLevel-2_Tables", data: jojaBundle},
+            {id: "boilerBundles", heading: "Boiler", className: "tableLevel-2_Tables", data: boilerBundles},
+            {id: "bulletinBunddles", heading: "Bulletin Board", className: "tableLevel-2_Tables", data: bulletinBunddles},
+            {id: "craftsBundles", heading: "Crafts", className: "tableLevel-2_Tables", data: craftsBundles},
+            {id: "fishBundles", heading: "Fish Tank", className: "tableLevel-2_Tables", data: fishBundles},
+            {id: "pantryBundles", heading: "Pantry", className: "tableLevel-2_Tables", data: pantryBundles},
+            {id: "vaultBunddles", heading: "Vault", className: "tableLevel-2_Tables", data: vaultBunddles},
+        ]
+   },
+
    travelingMerchant:
    {
         heading: "Traveling Merchant",
