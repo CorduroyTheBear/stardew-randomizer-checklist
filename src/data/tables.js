ebb895b5f6bf7676cb7af283d6311c9cafee51ed
CorduroyTheBear <candyunicornsnumber1@gmail.com>
2026-05-26 20:14:45 -0400

    Work from 5/26

--- src/data/tables.js
+++ src/data/tables.js
@@ -1,15 +1,45 @@
 // Import .json files for tables
 
-import raccoonChecks from "./raccoonRequestChecks.json";
+// Crop Sanity
+import crops from "./JsonFiles/CropSanity/crops.json";
+import fruit from "./JsonFiles/CropSanity/fruit.json";
+
+import minesTreasure from "./JsonFiles/minesTreasure.json";
+import raccoonChecks from "./JsonFiles/raccoonRequestChecks.json";
 
 // Create/Export tables
 
 export const TABLES = 
 {
+    // Checks on by default
     raccoonChecks:
     {
         heading: "Racoon",
         data: raccoonChecks,
-        className: "tableStyle_Raccoon"
-    }
+        className: "tableLevel-1"
+    },
+    minesTreasure:
+    {
+        heading: "Mines Chests",
+        data: minesTreasure,
+        className: "tableLevel-1"
+    },
+
+
+    // Other stuff (edit name later for clarity)
+    
+    /*
+    cropSanity:
+        {
+            heading: "Crops",
+            data: crops,
+            className: "tableLevel-1"
+        }
+    */
 };
+
+
+// To Add more tables
+
+// 1. Put .json file in ./data/JsonFiles
+// 2. Export table (shown above)
